6484f4edada28f6a9eaea621bde14106a46b45af
chanphy <chenzhanghui@co-mall.com>
2022-04-24 11:01:30 +0800

    删除templates

--- templates/taro-template/src/design/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { CustomerSwiper } from './customer-swiper';
-export { CustomerText } from './customer-text';
-export { CustomerNav } from './customer-nav';
-export { CustomerBanner } from './customer-banner';
--- templates/taro-template/src/services/index.ts
+++
@@ -1,3 +0,0 @@
-export { IframeManager } from './iframe-manager';
-export { api } from './api';
-export { getQueryVariable } from './get-query-variable';
